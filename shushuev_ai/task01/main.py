5aaf3ef7d70f0b941167cd6357dc7eed3bcd0245
statizarm <statiz.arm@gmail.com>
2021-03-14 19:24:27 +0300

    task01 - Написал функцию загружающую из файла лабиринт - Добавил .gitignore для игнора venv и .idea

--- shushuev_ai/task01/main.py
+++ shushuev_ai/task01/main.py
@@ -1,35 +1,20 @@
-import numpy as np
-import os
 import sys
+from typing import List
 
-def path_func(path1, path2):
-    path = os.path.join(path1, path2)
-    return (path, len(path))
+
+def read_map_from_csv(filename: str) -> List[List[int]]:
+    file = open(filename, "rt")
+
+    return [[int(num) for num in line.split(" ")] for line in file]
+
 
 def main(args):
-    x = 10
-    y = 1.0
-    output = f'x={x}, y={y}, sum={x + y}'
-    print(output)
+    filename = args[1] if len(args) > 1 else input("Enter path to csv file: ")
 
-    path = os.path.join('home', 'art')
+    m = read_map_from_csv(filename)
 
-    print(path)
+    print(m)
 
-    #Медленно, поэтому не надо
-    result = ''
-    for it in args:
-        result += it
-
-    print(f'result={result}')
-
-    #Как надо
-    effective_result = "".join(args)
-
-    print(f'efferctive_result={effective_result}')
-
-    p, l = path_func('/home','arg')
-    print(f'path={p}, length={l}')
 
 if __name__ == '__main__':
     main(sys.argv)
