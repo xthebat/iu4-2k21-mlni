9bfd4d4230bb0bd4faa1044c12f349538d889bfd
Sinyutina Darina <dsin76@yandex.ru>
2021-03-20 21:16:00 +0300

    task01
    - Добавила комментарии к алгоритму построения не оптимизированной матрицы смежности
    - Зарефакторила алгоритм построения не оптимизированной матрицы смежности:
    -- Убрала цикл предварительной обработки
    --  Изменила тип идентификатора вершины на кортеж координат
    - Добавила функцию оптимизации графа, построенной на обходе в ширину
    - Изменила обработку неверного числа аргументов программы:
    -- Теперь программа ругается и завершается со статусом -1 (255) 
    - Добавила юнит тесты:
    -- Тест на чтение карты лабиринта из файла
    -- Тест на создание графа
    -- Тест на упрощение ациклического графа
    -- Тест на упрощение графа (может содержать циклы)
    -- Тест на упрощение графа с начальной вершиной, принадлежащей простому пути
    -- Тест на упрощение графа с конечной вершиной, принадлежащей простому пути

--- sinyutina_ds/task01/main.py
+++ sinyutina_ds/task01/main.py
@@ -1,5 +1,5 @@
 import sys
-from typing import List, Dict
+from typing import List
 
 
 def parse_maze_map(file) -> List[List[int]]:
@@ -11,70 +11,82 @@ def read_maze_map(path: str) -> List[List[int]]:
         return parse_maze_map(file)
 
 
-def make_maze_graph(maze_map: List[List[int]]) -> Dict[str, Dict[str, int]]:
-    prev = 0
-    vertex_id = 1
-
+def make_maze_graph(maze_map: List[List[int]]):
+    # Функция обходит построчно таблицу. Поскольку для построения неоптимизированной матрицы смежности
+    # нам не нужна информация об сосдених ячейках на следующей или текущей строки (мы и так их посетим)
+    # построение графа исходит из занесения в матрицу смежности узла по предшествующему узлу.
     adj = dict()
-    vertex_map = [['' for i in range(len(row))] for row in maze_map]
-
-    for i in range(len(maze_map[0])):
-        if maze_map[0][i] == 1:
-            curr_vertex_id = str(vertex_id)
-            vertex_map[0][i] = curr_vertex_id
-            adj[curr_vertex_id] = dict()
-
-            if prev == 1:
-                prev_vertex_id = vertex_map[0][i - 1]
-                adj[curr_vertex_id][prev_vertex_id] = 1
-                adj[prev_vertex_id][curr_vertex_id] = 1
-
-            vertex_id += 1
-
-        prev = maze_map[0][i]
-
-    for i in range(1, len(maze_map)):
-        prev = 0
-        for j in range(len(maze_map[i])):
-            if maze_map[i][j] == 1:
-                curr_vertex_id = str(vertex_id)
-                vertex_map[i][j] = curr_vertex_id
+    for i, row in enumerate(maze_map):
+        prev = 0  # prev нужен для того чтобы не проверять j на 0 и хранить значение предыдущей в строке ячейки
+        for j, val in enumerate(row):
+            if val == 1:
+                curr_vertex_id = (i, j)
                 adj[curr_vertex_id] = dict()
 
                 if prev == 1:
-                    prev_vertex_id = vertex_map[i][j - 1]
+                    prev_vertex_id = (i, j - 1)
 
                     adj[curr_vertex_id][prev_vertex_id] = 1
                     adj[prev_vertex_id][curr_vertex_id] = 1
 
-                if maze_map[i - 1][j] == 1:
-                    prev_vertex_id = vertex_map[i - 1][j]
+                if i > 0 and maze_map[i - 1][j] == 1:
+                    prev_vertex_id = (i - 1, j)
 
                     adj[curr_vertex_id][prev_vertex_id] = 1
                     adj[prev_vertex_id][curr_vertex_id] = 1
 
-                vertex_id += 1
-
-            prev = maze_map[i][j]
-
+            prev = val
     return adj
 
 
+def simplify_paths(adj, start, end):
+    nodes_queue = [(start, start, 0)]  # в queue содержится (предудщий узел, текущая вершина, вес простого пути)
+    # предыдущий узел start для случая когда стартовый узел имеет число соседей == 1
+
+    visited = dict()
+    new_adj = dict()
+    # Обходим по всем элементам в очереди
+    for prev, curr, weight in nodes_queue:
+        visited[curr] = True
+
+        # получаем список непосещенных соседей
+        neighbors = list(filter(lambda kv: True if visited.get(kv[0]) is None else False, adj[curr].items()))
+
+        # Сравнение по end исключает ситуацию, когда end - промежуточная вершина в простом пути
+        if len(neighbors) != 1 or curr == end:
+            if prev != curr:
+                if new_adj.get(prev) is None:
+                    new_adj[prev] = {curr: weight}
+                else:
+                    new_adj[prev][curr] = weight
+
+                if new_adj.get(curr) is None:
+                    new_adj[curr] = {prev: weight}
+                else:
+                    new_adj[curr][prev] = weight
+
+            weight = 0
+            prev = curr
+
+        for k, v in neighbors:
+            nodes_queue.append((prev, k, weight + v))
+
+    return new_adj
+
+
 def main(args):
-    if len(args) < 2:
-        print("введите путь до файла c лабиринтом: ")
-        path = input()
-    elif len(args) > 3:
-        print("слишком много аргументов, введите название файла: ")
-        path = input()
-    else:
-        path = args[1]
+    if len(args) != 2:
+        print("В аргументах ожидается путь до файла с лабиринтом")
+        exit(-1)
+
+    path = args[1]
 
     maze_map = read_maze_map(path)
 
     adj = make_maze_graph(maze_map)
 
-    print(maze_map)
+    adj = simplify_paths(adj, (0, 0), (len(maze_map) - 1, len(maze_map[-1]) - 1))
+
     print(adj)
 
 
---
+++ sinyutina_ds/task01/test.py
@@ -0,0 +1,106 @@
+import unittest
+import main
+
+
+class Tests(unittest.TestCase):
+    def test_read_maze(self):
+        expected = [
+            [1, 1, 0, 1, 1, 1],
+            [0, 1, 0, 1, 0, 0],
+            [0, 1, 0, 1, 1, 1],
+            [0, 1, 1, 1, 0, 1],
+            [0, 1, 0, 0, 0, 1],
+            [0, 1, 1, 1, 0, 1]
+        ]
+        received = main.read_maze_map('maze.txt')
+        self.assertEqual(expected, received)
+
+    def test_build_graph(self):
+        maze = [
+            [1, 1, 1],
+            [0, 1, 0],
+            [0, 1, 1]
+        ]
+
+        expected = {
+            (0, 0): {(0, 1): 1},
+            (0, 1): {(0, 0): 1, (0, 2): 1, (1, 1): 1},
+            (0, 2): {(0, 1): 1},
+            (1, 1): {(0, 1): 1, (2, 1): 1},
+            (2, 1): {(1, 1): 1, (2, 2): 1},
+            (2, 2): {(2, 1): 1},
+        }
+        received = main.make_maze_graph(maze)
+        self.assertEqual(expected, received, msg=f'expected:\n{expected}\nreceived:\n{received}')
+
+    def test_simplify_uncycled_paths(self):
+        maze = [
+            [1, 1, 1],
+            [0, 1, 0],
+            [0, 1, 1]
+        ]
+
+        expected = {
+            (0, 0): {(0, 1): 1},
+            (0, 1): {(0, 0): 1, (0, 2): 1, (2, 2): 3},
+            (0, 2): {(0, 1): 1},
+            (2, 2): {(0, 1): 3}
+        }
+
+        received = main.simplify_paths(main.make_maze_graph(maze), (0, 0), (2, 2))
+
+        self.assertEqual(expected, received)
+
+    def test_simplify_cycled_paths(self):
+        maze = [
+            [1, 1, 1, 1],
+            [0, 1, 0, 1],
+            [0, 1, 1, 1],
+            [0, 0, 1, 0],
+            [0, 0, 1, 1]
+        ]
+
+        expected = {
+            (0, 0): {(0, 1): 1},
+            (0, 1): {(0, 0): 1, (2, 2): 3, (2, 3): 4},
+            (2, 2): {(0, 1): 3, (4, 3): 3, (2, 3): 1},
+            (2, 3): {(0, 1): 4, (2, 2): 1},
+            (4, 3): {(2, 2): 3}
+        }
+
+        received = main.simplify_paths(main.make_maze_graph(maze), (0, 0), (4, 3))
+        self.assertEqual(expected, received)
+
+    def test_simplify_paths_with_cycled_start(self):
+        maze = [
+            [1, 1, 1, 1],
+            [1, 0, 0, 1],
+            [1, 0, 0, 0],
+            [1, 1, 1, 1],
+        ]
+
+        expected = {
+            (0, 0): {(3, 3): 6, (1, 3): 4},
+            (3, 3): {(0, 0): 6},
+            (1, 3): {(0, 0): 4}
+        }
+
+        received = main.simplify_paths(main.make_maze_graph(maze), (0, 0), (3, 3))
+        self.assertEqual(expected, received)
+
+    def test_simplify_paths_with_cycled_end(self):
+        maze = [
+            [1, 0, 1, 1],
+            [1, 0, 0, 1],
+            [1, 0, 0, 1],
+            [1, 1, 1, 1],
+        ]
+
+        expected = {
+            (0, 0): {(3, 3): 6},
+            (3, 3): {(0, 0): 6, (0, 2): 4},
+            (0, 2): {(3, 3): 4}
+        }
+
+        received = main.simplify_paths(main.make_maze_graph(maze), (0, 0), (3, 3))
+        self.assertEqual(expected, received)
