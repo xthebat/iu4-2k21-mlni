cd1fc6869afead0921af966067b1dc0f31392d9a
statizarm <statiz.arm@gmail.com>
2021-03-21 14:23:11 +0300

    task01 - Заменил предыдущую версию генерации графа на новую: -- Идея заключается в использовании волнового алгоритма для обхода лабиринта и если вершина имеет отличное 1 колическо непосещенных еще соседей она заносится в матрицу смежности - Добавил тесты: -- Тест на чтение лабиринта из файла -- Тест на построение графа для простого случая -- Тест на построение графа для лабиринта с островом -- Тест на построение графа для лабиринта с несколькими узлами -- Тест на построение графа для лабиринта с циклическим путем

--- shushuev_ai/task01/main.py
+++ shushuev_ai/task01/main.py
@@ -1,55 +1,64 @@
 import sys
-from typing import List, Dict, Generator
+import numpy as np
+from typing import List, Tuple
 
 
-def generate_vertexes_from_file(filepath):
-    file = open(filepath, "rt")
-    row_gen = ([int(num) for num in line.split(' ')] for line in file)
-
-    prev_row = None
-    vid = 1
-    for row in row_gen:
-        prev = 0
-        for i in range(len(row)):
-            if row[i] == 1:
-                row[i] = str(vid)
-                vid += 1
-
-                if prev == 1:
-                    yield str(row[i - 1]), str(row[i])
-
-                if prev_row is not None and prev_row[i] != 0:
-                    yield str(prev_row[i]), str(row[i])
-
-                prev = 1
-            else:
-                prev = 0
-
-        prev_row = row
+def read_maze_from_file(filepath: str) -> List[List[int]]:
+    with open(filepath, "rt") as file:
+        return [[int(num) for num in line.split(' ')] for line in file]
 
 
-def build_graph_from_file_map(filepath) -> Dict[str, Dict[str, int]]:
-    gen = generate_vertexes_from_file(filepath)
+def neighbours(maze, node):
+    for drow, dcol in (0, 1), (1, 0), (0, -1), (-1, 0):
+        neighbour_row, neighbour_col = node
+        neighbour_row += drow
+        neighbour_col += dcol
+        if 0 <= neighbour_row < len(maze) and 0 <= neighbour_col < len(maze[neighbour_row]):
+            yield neighbour_row, neighbour_col
 
+
+def build_graph_from_maze_map(maze: List[List[int]], start: Tuple[int, int], end: Tuple[int, int]):
+    # в очереди хранятся пары корень, текущий узел
+    queue = [(start, start)]
+    weight_map = [[np.nan for _ in row] for row in maze]
+    weight_map[start[0]][start[1]] = 0
+    visited = dict()
     adj = dict()
-    for prev, curr in gen:
-        if adj.get(prev) is None:
-            adj[prev] = {curr: 1}
-        else:
-            adj[prev][curr] = 1
 
-        # if adj.get(curr) is None:
-            # adj[curr] = {prev: 1}
-        # else:
-            # adj[curr][prev] = 1
+    for root, node in queue:
+        visited[node] = True
+        unvisited = list(filter(lambda x: True if visited.get(x) is None and maze[x[0]][x[1]] == 1 else False,
+                                neighbours(maze, node)))
+
+        if len(unvisited) != 1 or node == end:
+            # получаем вес ребра
+            length = weight_map[node[0]][node[1]] - weight_map[root[0]][root[1]]
+            if adj.get(root) is None:
+                adj[root] = {node: length}
+            else:
+                adj[root][node] = length
+
+            root = node
+
+        for neighbour in unvisited:
+            row, col = neighbour
+            if maze[row][col] == 1:
+                queue.append((root, neighbour))
+                weight_map[row][col] = weight_map[node[0]][node[1]] + 1
 
     return adj
 
 
 def main(args):
-    filename = args[1] if len(args) > 1 else input("Enter path to csv file: ")
+    if len(args) != 2:
+        print("expected a path to maze file as first argument")
+        exit(-1)
 
-    adj = build_graph_from_file_map(filename)
+    filename = args[1]
+
+    maze = read_maze_from_file(filename)
+
+    adj = build_graph_from_maze_map(maze, (0, 0), (len(maze) - 1, len(maze[-1]) - 1))
 
     print(adj)
 
---
+++ shushuev_ai/task01/test.py
@@ -0,0 +1,77 @@
+import unittest
+import main
+
+
+class Test(unittest.TestCase):
+    def test_read_maze(self):
+        filename = 'test.csv'
+        expected = [
+            [1, 0, 0, 1],
+            [1, 1, 1, 1],
+            [0, 0, 1, 0],
+            [1, 1, 1, 1]
+        ]
+        received = main.read_maze_from_file(filename)
+        self.assertEqual(expected, received)
+
+    def test_build_graph_simple_case(self):
+        maze = [
+            [1, 0, 0, 0],
+            [1, 1, 1, 0],
+            [0, 0, 1, 0],
+            [0, 0, 1, 1]
+        ]
+
+        expected = {
+            (0, 0): {(3, 3): 6}
+        }
+        received = main.build_graph_from_maze_map(maze, (0, 0), (3, 3))
+        self.assertEqual(expected, received)
+
+    def test_build_graph_island_case(self):
+        maze = [
+            [1, 0, 0, 0],
+            [1, 1, 1, 0],
+            [0, 0, 1, 0],
+            [1, 0, 1, 1]
+        ]
+
+        expected = {
+            (0, 0): {(3, 3): 6}
+        }
+        received = main.build_graph_from_maze_map(maze, (0, 0), (3, 3))
+        self.assertEqual(expected, received)
+
+    def test_build_graph_mult_nodes_case(self):
+        maze = [
+            [1, 0, 1, 1],
+            [1, 1, 1, 0],
+            [1, 0, 1, 0],
+            [1, 0, 1, 1]
+        ]
+
+        expected = {
+            (0, 0): {(1, 0): 1},
+            (1, 0): {(3, 0): 2, (1, 2): 2},
+            (1, 2): {(0, 3): 2, (3, 3): 3}
+        }
+        received = main.build_graph_from_maze_map(maze, (0, 0), (3, 3))
+        self.assertEqual(expected, received)
+
+    def test_build_graph_cycled_path(self):
+        maze = [
+            [1, 0, 1, 1, 1],
+            [1, 0, 1, 0, 1],
+            [1, 1, 1, 0, 1],
+            [1, 0, 0, 0, 1],
+            [1, 1, 1, 1, 1]
+        ]
+
+        expected = {
+            (0, 0): {(2, 0): 2},
+            (2, 0): {(4, 4): 6, (2, 4): 8},
+            (4, 4): {(2, 4): 2}
+        }
+        received = main.build_graph_from_maze_map(maze, (0, 0), (4, 4))
+        self.assertEqual(expected, received)
+
